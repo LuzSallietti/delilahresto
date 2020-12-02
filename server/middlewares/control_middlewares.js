3e86aa541618f09fae0dc31498239a144da37334
LuzSallietti <61067366+LuzSallietti@users.noreply.github.com>
2020-12-02 16:32:43 -0300

    Update control_middlewares.js

--- server/middlewares/control_middlewares.js
+++ server/middlewares/control_middlewares.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require("jsonwebtoken");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('mysql://root@localhost:3306/delilahtest');
+const sequelize = new Sequelize('mysql://root@localhost:3306/delilah');
 
 function controlProductData (req, res, next){
     if (req.body.name && req.body.price){
